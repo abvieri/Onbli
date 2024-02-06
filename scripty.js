cd9c15efe493b884d4dd83e093d06abff4744800
abvieri <vieriabraao@gmail.com>
2024-02-06 20:48:30 -0300

    Novo Design

--- scripty.js
+++ scripty.js
@@ -1,13 +1,37 @@
-/*Notificação*/
+// Conheça á Onbli Animação
 
-const but = document.getElementById('but');
-  console.log(but); 
-    
-  but.addEventListener("click", function() {
-    alert("Enviado");
-    const txtCenter = document.getElementById('txtCenter');
-    console.log(txtCenter);
+window.addEventListener('load', verificacao);
+var sobre_document = document.getElementsByClassName("sobre-document");
+var marca_pagina = document.getElementsByClassName("marca-pagina");
+let sobre = Array.from(sobre_document);
+let pagina = Array.from(marca_pagina);
 
-    txtCenter.textContent = "teste";
+function verificacao() {
+    sobre.forEach(e => {
+        if (e.classList[3] === "active") {
+            e.getElementsByClassName('texto-pagina')[0].style.display = "flex";
+            e.getElementsByClassName('marca-pagina')[0].classList.remove('col-12');
+            e.getElementsByClassName('marca-pagina')[0].classList.add('col-1');
+            e.getElementsByClassName('texto-pagina')[0].style.animation= '.1s ease-in both running texto-pagina';
+        } else {
+            e.getElementsByClassName('texto-pagina')[0].style.display = 'none';
+            e.getElementsByClassName('marca-pagina')[0].classList.remove('col-1');
+            e.getElementsByClassName('marca-pagina')[0].classList.add('col-12');
+        }
+    });
+}
+
+pagina.forEach(e => {
+    e.addEventListener('click', function () {
+        var element = e.parentNode.classList[3]
+        if (element != "active") {
+            pagina.forEach(el => {
+                if (el.parentNode.classList[3] === "active") {
+                    el.parentNode.classList.remove("active");
+                }
+            });
+            e.parentNode.classList.add('active');
+            verificacao();
+        }
+    })
 });
-
